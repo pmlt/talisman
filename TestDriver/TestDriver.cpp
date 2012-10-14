ada916e8761afaad614147cbaff57ba1e3a7f21a
Philippe Milot <philmilot@gmail.com>
2012-10-14 12:51:22 -0400

    Test program

--- TestDriver/TestDriver.cpp
+++ TestDriver/TestDriver.cpp
@@ -2,10 +2,32 @@
 //
 
 #include "stdafx.h"
+#include "TalismanGame.h"
+#include <iostream>
+
+using namespace std;
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+  Warrior war = Warrior();
+  Wizard wiz = Wizard();
+  Sword sword = Sword();
+  Axe axe = Axe();
+  Shield shield = Shield();
+  Mule mule = Mule();
+
+  cout << "Warrior has strength of: " << war.strength() << endl;
+  cout << "Wizard has strength of: " << wiz.strength() << endl;
+  
+  // Equip characters
+  war.pickup(&axe);
+  war.pickup(&shield);
+  wiz.pickup(&sword);
+  wiz.pickup(&mule);
+
+  cout << "Warrior has strength of: " << war.strength() << endl;
+  cout << "Wizard has strength of: " << wiz.strength() << endl;
 	return 0;
 }
 
