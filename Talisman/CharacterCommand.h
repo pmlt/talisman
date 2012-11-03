9fd453fb260ce4dbdb6cd51339bf604c6519fcc6
Philippe Milot <philmilot@gmail.com>
2012-11-03 14:34:21 -0400

    Some documentation

--- Talisman/CharacterCommand.h
+++ Talisman/CharacterCommand.h
@@ -3,19 +3,37 @@
 
 class Character; //Temporary declaration just to compile
 
+/*
+  Class: CharacterCommand
+  Represents a single reversible operation on a character.
+*/
 class CharacterCommand
 {
 public:
   CharacterCommand(Character &subject);
   virtual ~CharacterCommand(void);
 
+  /*
+    Method: execute
+    Apply the operation to the character.
+  */
   virtual void execute() = 0;
+
+  /*
+    Method: revert
+    Undo the operation to the character.
+  */
   virtual void revert() = 0;
 
 protected:
   Character *subject;
 };
 
+/*
+  Class: GiveItem
+  Give an item to a character. Undoing this operation
+  remove the item from the character.
+*/
 class GiveItem : public CharacterCommand
 {
 public:
@@ -27,6 +45,11 @@ private:
   Item *item;
 };
 
+/*
+  Class: GiveTalisman
+  Give a talisman to a character. Undoing this operation
+  removes the talisman from the character.
+*/
 class GiveTalisman : public CharacterCommand
 {
 public:
@@ -38,6 +61,11 @@ private:
   Talisman *talisman;
 };
 
+/*
+  Class: DropItem
+  Remove an item from a character's inventory. Undoing this
+  operation will give the item back to the character.
+*/
 class DropItem : public CharacterCommand
 {
 public:
@@ -49,6 +77,11 @@ private:
   Item *item;
 };
 
+/*
+  Class: DropTalisman
+  Remove a talisman from a character's inventory. Undoing this
+  operation will give the talisman back to the character.
+*/
 class DropTalisman : public CharacterCommand
 {
 public:
@@ -60,6 +93,11 @@ private:
   Talisman *talisman;
 };
 
+/*
+  Class: TransformIntoToad
+  Transform a character into a toad. Undoing this operation
+  will turn him back into a human.
+*/
 class TransformIntoToad : public CharacterCommand
 {
 public:
@@ -69,6 +107,11 @@ public:
   virtual void revert();
 };
 
+/*
+  Class: Move
+  Move a character to a new tile. Undoing this operation
+  will move the character back to the previous tile.
+*/
 class Move : public CharacterCommand
 {
 public:
