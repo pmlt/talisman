e419765abcc05c52b88777d2e42369cf3a0f14e3
Philippe Milot <philmilot@gmail.com>
2012-11-04 12:56:25 -0500

    Add missing parts to game design

--- Talisman/MapTile.cpp
+++ Talisman/MapTile.cpp
@@ -2,11 +2,30 @@
 #include "MapTile.h"
 
 
-MapTile::MapTile(void)
+MapTile::MapTile(string title) :
+  title(title),top(NULL),right(NULL),bottom(NULL),left(NULL),inward_bridge(NULL),outward_bridge(NULL)
 {
 }
 
 
 MapTile::~MapTile(void)
 {
+}
+
+string MapTile::getTitle() const { return this->title; }
+
+void MapTile::setNeighbors(MapTile* top, MapTile* right, MapTile* bottom, MapTile* left)
+{
+  this->top = top;
+  this->right = right;
+  this->bottom = bottom;
+  this->left = left;
+}
+void MapTile::setInwardBridge(MapTile* bridge) { this->inward_bridge = bridge; }
+void MapTile::setOutwardBridge(MapTile* bridge) { this->outward_bridge = bridge; }
+
+unsigned char MapTile::rollMovement()
+{
+  //Random number between 1 and 6
+  return 1;
 }
--- Talisman/MapTile.h
+++ Talisman/MapTile.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <string>
+
+using namespace std;
+
 /*
   Class: MapTile
   Represents a location on the board. Dummy class to help model Character for now.
@@ -7,7 +11,44 @@
 class MapTile
 {
 public:
-  MapTile(void);
+  MapTile(string title);
   virtual ~MapTile(void);
+
+  virtual string getTitle() const;
+  
+  void setNeighbors(MapTile* top, MapTile* right, MapTile* bottom, MapTile* left);
+  void setInwardBridge(MapTile* bridge);
+  void setOutwardBridge(MapTile* bridge);
+
+  unsigned char rollMovement();
+
+private:
+  string title;
+  MapTile* top;
+  MapTile* left;
+  MapTile* bottom;
+  MapTile* right;
+  MapTile* inward_bridge;
+  MapTile* outward_bridge;
 };
 
+class OuterMapTile : public MapTile
+{
+public:
+  OuterMapTile();
+  virtual ~OuterMapTile();
+};
+
+class MiddleMapTile : public MapTile
+{
+public:
+  MiddleMapTile();
+  virtual ~MiddleMapTile();
+};
+
+class InnerMapTile : public MapTile
+{
+public:
+  InnerMapTile();
+  virtual ~InnerMapTile();
+};
