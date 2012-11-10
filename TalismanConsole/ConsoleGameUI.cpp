7b7c15b16123ab6df7f84a11bddec7c08f829233
Brett F <brett.com@gmail.com>
2012-11-10 16:41:28 -0500

    Added ConsoleGameUI's Prompt method
    I've got 6 errors when compiling in AdventureCard.cpp, down from 37
    after the changes in AdventureCard.h. Could it be something to do with
    VS2010?

--- TalismanConsole/ConsoleGameUI.cpp
+++ TalismanConsole/ConsoleGameUI.cpp
@@ -1,6 +1,6 @@
 #include "stdafx.h"
 #include "ConsoleGameUI.h"
-
+#include <iostream>
 
 ConsoleGameUI::ConsoleGameUI(Game &game) :
   GameUI(game)
@@ -14,8 +14,32 @@ ConsoleGameUI::~ConsoleGameUI(void)
 
 unsigned int prompt(string msg, string options[], unsigned int options_size)
 {
-  //Use cout and cin to display choices and retrieve user input.
-  //Should re-ask if user did not choose one of the options.
+  //asks user for the index position of their choice plus 1, in
+  //order to allow for the choice of 1, 2, 3, instead of 0, 1, 2
+  
+  int choice;
+
+  cout << msg << endl;
+  for (int i = 0; i < options_size; ++i)
+  {
+    cout << "  " << i+1 << ") " << options[i] << endl;
+  }
+  cout << "Enter your choice: ";
+  cin >> choice;
+
+  while (choice < 1 || choice > options_size)
+  {
+    cout << "Invalid choice selected. Please enter a number between 1 and " << options_size << "." << endl;
+    cout << msg << endl;
+    for (int i = 0; i < options_size; ++i)
+    {
+      cout << " " << i+1 << ") " << options[i] << endl;
+    }
+    cout << "Enter your choice: ";
+    cin >> choice;
+  }
+
+  return (choice - 1);
 }
 
 void ConsoleGameUI::notify()
