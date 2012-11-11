7a02c7f1d2cefeb01b0f971c9b33c77264139d6f
Philippe Milot <philmilot@gmail.com>
2012-11-11 15:06:28 -0500

    Add message method to TException

--- Talisman/TException.cpp
+++ Talisman/TException.cpp
@@ -1,6 +1,7 @@
 #include "StdAfx.h"
 #include "TException.h"
 
+using namespace std;
 
 TException::TException(void)
 {
@@ -9,4 +10,17 @@ TException::TException(void)
 
 TException::~TException(void)
 {
+}
+
+string TException::message() { return "Generic error."; }
+string NotInInventoryException::message() { return "The item was not found in the inventory."; }
+string InventoryFullException::message() { return "This character's inventory is full."; }
+string PlayerNotFoundException::message() { return "Player not found."; }
+string PlayerExistsException::message() { return "This player is already in the game."; }
+
+InvalidMapFileException::InvalidMapFileException() {}
+InvalidMapFileException::InvalidMapFileException(string l) : line(l) {}
+string InvalidMapFileException::message() {
+  if (this->line.size() > 0) return "The provided map file has an error on line: " + this->line;
+  else return "The provided map file was invalid.";
 }
--- Talisman/TException.h
+++ Talisman/TException.h
@@ -1,4 +1,7 @@
 #pragma once
+#include <string>
+
+using namespace std;
 
 /*
   Class: TException
@@ -9,6 +12,8 @@ class TException
 public:
   TException(void);
   virtual ~TException(void);
+
+  virtual string message();
 };
 
 /*
@@ -16,31 +21,55 @@ public:
   Occurs when a character attempts to drop something that he
   is not carrying.
 */
-class NotInInventoryException : public TException {};
+class NotInInventoryException : public TException
+{
+public:
+  virtual string message();
+};
 
 /*
   Class: InventoryFullException
   Occurs when a character attempts to pickup an object when
   his inventory is already full.
 */
-class InventoryFullException : public TException {};
+class InventoryFullException : public TException
+{
+public:
+  virtual string message();
+};
 
 /*
   Class: PlayerNotFoundException
   Occurs when the game tries to remove a player that is not
   currently playing.
 */
-class PlayerNotFoundException : public TException {};
+class PlayerNotFoundException : public TException
+{
+public:
+  virtual string message();
+};
 
 /*
   Class: PlayerExistsException
   Occurs when the game tries to add a player that is
   already currently playing.
 */
-class PlayerExistsException : public TException {};
+class PlayerExistsException : public TException
+{
+public:
+  virtual string message();
+};
 
 /*
   Class: InvalidMapFileException
   Occurs whenever a bad file was passed to Board::from_file.
 */
-class InvalidMapFileException : public TException {};
+class InvalidMapFileException : public TException
+{
+public:
+  InvalidMapFileException();
+  InvalidMapFileException(string line);
+  virtual string message();
+private:
+  string line;
+};
